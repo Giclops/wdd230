c9216f9bc157a3c53d004bbd54f3574e55e73ffb
Giclops <atchleyne@gmail.com>
2022-02-05 18:07:00 -0700

    some chamber site changes

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -1,5 +1,6 @@
 const menubutton = document.querySelector('.menu');
 const mainnav = document.querySelector('nav');
+const monTuesBanner = document.querySelector('.banner');
 
 menubutton.addEventListener('click', () => {
     mainnav.classList.toggle('responsive')
@@ -7,8 +8,22 @@ menubutton.addEventListener('click', () => {
 
 window.onload = 
 function datesNStuff() {    
-    let year = new Date().getFullYear();
+    let date = new Date();
+    let year = date.getFullYear();
     let updated = "Last Updated: " + document.lastModified;
     document.getElementById("lastUpdate").innerText = updated;
     document.querySelector('#copyright').innerText = year;
+
+    let day = date.getDay();
+    if (day == 6 || day == 3) {
+        monTuesBanner.classList.toggle('bannerShow');
+    }
+}
+
+window.onload = 
+function bannerShow() {
+    let date = new Date.getDay();
+    if (date == 6 || date == 3) {
+        monTuesBanner.classList.toggle('bannerShow');
+    }
 }
